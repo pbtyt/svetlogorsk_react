38346c8e0caa39905ec7009d8cc2913d6fa92681
bladee <darkyt9@gmail.com>
2023-10-23 18:13:53 +0300

    add: search func

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -7,40 +7,42 @@ import villageData from '../../data/villages.json'
 
 const SearchPage = () => {
     const [searchValue, setSearchValue] = useState('')
+    const [query, setQuery] = useState('')
+    const [data, setData] = useState([])
 
-    useEffect(()=>{
-        console.log(searchValue)
-    }, [searchValue])
+    useEffect(() => {
+        setData(villageData.filter(el => el.name.toLowerCase().includes(query.toLowerCase())))
+    }, [query])
 
-  return (
-    <div className={styles.wrapper}>
-        <div className={styles.searchInputWrapper}>
-            <input type="text" value={searchValue} onChange={ e => setSearchValue(e.target.value) }/>
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.searchInputWrapper}>
+                <input type="text" value={searchValue} onChange={e => setSearchValue(e.target.value)} />
 
-            <button>Искать</button>
+                <button onClick={() => setQuery(searchValue)}>Искать</button>
+            </div>
+
+            <div className={styles.searchInfo}>
+                <h3>Всего найдено: {data.length}</h3>
+
+                <table>
+                    <tr>
+                        <th>Название</th>
+                        <th>Год Разрушения</th>
+                        <th>Домов до войны</th>
+                        <th>Людей до войны</th>
+                        <th>Разрушено Домов</th>
+                        <th>Убито людей</th>
+                    </tr>
+
+                    {
+                        data.map((data, index) => <TableRow data={data} key={index} />)
+                    }
+
+                </table>
+            </div>
         </div>
-
-        <div className={styles.searchInfo}>
-            <h3>Всего найдено: 77</h3>
-
-            <table>
-                <tr>
-                    <th>Название</th>
-                    <th>Год Разрушения</th>
-                    <th>Домов до войны</th>
-                    <th>Людей до войны</th>
-                    <th>Разрушено Домов</th>
-                    <th>Убито людей</th>
-                </tr>
-
-                {
-                    villageData.map( data => <TableRow data={data}/> )
-                }
-
-            </table>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default SearchPage
