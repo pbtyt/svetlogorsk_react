4222077d04df918e664b4cff14d8c45babe0a146
pbtyt <119056740+pbtyt@users.noreply.github.com>
2024-01-29 20:27:50 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,19 +6,13 @@ const Footer = () => {
         
         <div className={styles.blockFooterWrapper}>
             <div className={styles.contacts}>
-                <h3>Контакты</h3>
-                <h4>+375293033229(nikitabobchenok.work@gmail.com)</h4>
-                <h4>+375291731643(Gleb.polyan1337@mail.ru)</h4>
-                <h4>+375447665850(korolukmx2@gmail.com)</h4>
             </div>
             
             <div className={styles.spacer}></div>
             
             <div className={styles.authors}>
-                <h3>Авторы (Учащиеся 11А класса "Средняя школа №12 г.Светлогорска")</h3>
+                <h3>Автор (Учащийся 11А класса "Средняя школа №12 г.Светлогорска")</h3>
                 <h4>Бобченок Никита</h4>
-                <h4>Полуян Глеб</h4>
-                <h4>Королюк Максим </h4>
             </div>
 
         </div>    
